dfb99731db526241e7223b0b07abe79d9ad4b575
gamingcoder2 <84407492+gamingcoder2@users.noreply.github.com>
2021-08-05 16:28:18 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+menu_list_array = ["Veg Margherita Pizza","Mutton Pizza","Pepperoni Pizza", "Veg tofu pizza"
+                    ];
+
+function getmenu(){
+var htmldata="";
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+        //give the appropriate id name as display_menu  
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+add_top();
+menu_list_array.sort();
+console.log(menu_list_array)
+        // use the sort function as - menu_list_array.sort();
+        htmldata="";
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+      }
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	
+}
